58b6ec2c4f8f87916bf50b7839dd1c372d7179bc
gogowhy <gogowhy@sjtu.edu.cn>
2019-07-02 10:42:12 +0800

    更新了PKBG的backend
    在里面加入server、sonsumer和user_client

---
+++ Code/后端spring cloud 试验开发/PKBG_backend/PKBG_consumer/src/main/java/com/pkbg/eurekaconsumer/EurekaConsumerApplication.java
@@ -0,0 +1,22 @@
+package com.pkbg.eurekaconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class EurekaConsumerApplication {
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(EurekaConsumerApplication.class).web(true).run(args);
+    }
+}
