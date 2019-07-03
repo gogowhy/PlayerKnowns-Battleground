63e5651b316bd9e9c801006a632cd7a5d93b4878
gogowhy <gogowhy@sjtu.edu.cn>
2019-07-03 22:03:13 +0800

    更新函数参数以及consumer分层

---
+++ Code/PKBG_backend/consumer/src/main/java/com/example/demo/Controller/UserController.java
@@ -0,0 +1,35 @@
+package com.example.demo.Controller;
+
+import com.example.demo.client.UserClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+
+@RestController
+public class UserController {
+    @Autowired
+    UserClient userClient;
+
+    @RequestMapping("/register")
+    public String register(HttpServletRequest request) {
+        return userClient.register(request);
+    }
+
+    @RequestMapping("/login")
+    public String login(HttpServletRequest request) {
+        return userClient.login(request);
+    }
+
+    @RequestMapping("/resetPass")
+    public String resetPass(HttpServletRequest request) {
+        return userClient.resetPass(request);
+    }
+
+    @RequestMapping("/test")
+    public String test()
+    {
+        return  userClient.test();
+    }
+}
--- 每日总结以及学习笔记/SPRING CLOUD代码及学习笔记/第三天完全联通/backend/consumer/src/main/java/com/example/demo/client/UserClient.java
+++ 每日总结以及学习笔记/SPRING CLOUD代码及学习笔记/第三天完全联通/backend/consumer/src/main/java/com/example/demo/client/UserClient.java
@@ -1,19 +1,22 @@
-package com.example.demo;
+package com.example.demo.client;
 
+import com.netflix.ribbon.proxy.annotation.Http;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.http.HttpServletRequest;
+
 @FeignClient("user-client")
 public interface UserClient {
     @RequestMapping("/user/register")
-    String register();
+    String register(HttpServletRequest request);
 
     @RequestMapping("/user/login")
-    String login();
+    String login(HttpServletRequest request);
 
     @RequestMapping("/user/resetPass")
-    String resetPass();
+    String resetPass(HttpServletRequest request);
 
     @RequestMapping("user/test")
     String test();
