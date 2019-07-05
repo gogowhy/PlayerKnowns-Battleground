dae9de5ea10fac0bc0a6707cc46b6787e63a08d3
QBL2333 <757994086@qq.com>
2019-07-05 16:25:32 +0800

    Websocket

--- Code/PKBG_backend/roomclient/src/main/java/com/example/demo/DaoImpl/RoomDaoImpl.java
+++ Code/PKBG_backend/roomclient/src/main/java/com/example/demo/DaoImpl/RoomDaoImpl.java
@@ -4,10 +4,12 @@ package com.example.demo.DaoImpl;
 import com.example.demo.Dao.RoomDao;
 import com.example.demo.Entity.Player;
 import com.example.demo.Entity.Room;
+import com.example.demo.Handler.MyHandler;
 import com.example.demo.Repository.PlayerRepository;
 import com.example.demo.Repository.RoomRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.socket.TextMessage;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.Iterator;
@@ -68,9 +70,12 @@ public class RoomDaoImpl implements RoomDao {
         Integer roomnumber=room.getRoomnumber();
         List<Player> players =playerRepository.findByRoomnumber(roomnumber);
 
+        MyHandler myHandler = new MyHandler();
         for(int i=0;i<players.size();i++)
         {
             Player player_temp=players.get(i);
+            String playername = player_temp.getPlayername();
+            myHandler.sendMessageToUser(playername, new TextMessage("Dismissed"));
             playerRepository.delete(player_temp);
         }
 
@@ -82,6 +87,7 @@ public class RoomDaoImpl implements RoomDao {
     @Override
     public String join(HttpServletRequest request){
         String roomnumber = request.getParameter("roomnumber");
+        String username = request.getParameter("username");
         Integer rmnumber = Integer.valueOf(roomnumber).intValue();
         Room room = new Room();
         room = roomRepository.findByRoomnumber(rmnumber);
@@ -97,11 +103,20 @@ public class RoomDaoImpl implements RoomDao {
         {
             return "Target Room Is Full!";
         }
+
+        MyHandler myHandler = new MyHandler();
+        List<Player> players =playerRepository.findByRoomnumber(rmnumber);
+        for(int i=0;i<players.size();i++)
+        {
+            Player player_temp=players.get(i);
+            String playername = player_temp.getPlayername();
+            myHandler.sendMessageToUser(playername, new TextMessage("Joined"+username));
+        }
+
         Integer newnumber = room.getPlayernumber()+1;
         room.setPlayernumber(newnumber);
         roomRepository.save(room);
 
-        String username = request.getParameter("username");
         Player player = new Player();
         player.setRoomnumber(rmnumber);
         player.setPlayername(username);
